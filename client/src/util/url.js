e3aecdab705f93c6ab662f94f2f1635064f394ad
Liang-Liu <82113781+Liang-Liu@users.noreply.github.com>
2021-09-25 08:52:15 -0700

    url update

--- client/src/util/url.js
+++ client/src/util/url.js
@@ -1,2 +1,2 @@
-// export const baseUrl = "https://ll-mern-blog-app.herokuapp.com/";
-export const baseUrl = "http://localhost:5000/";
+export const baseUrl = "https://ll-mern-blog-app.herokuapp.com/";
+// export const baseUrl = "http://localhost:5000/";
